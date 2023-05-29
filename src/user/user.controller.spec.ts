7419e1d8e89c833c0e05796b91179d0b5d1e5378
Vinicius <viniciusbaldessar31@gmail.com>
2023-05-29 17:41:49 -0300

    ajustando testes

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 describe('UserController', () => {
   let controller: UserController;
+  let fakeUserService: Partial<UserService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeAuthService = {
+      signup: (email: string, password: string) => {
+        return Promise.resolve({id:1, email, password} as User)
+      },
+      // signin: () => {}
+    }
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers:[
+        {
+          provide: AuthService,
+          useValue:fakeAuthService,
+        },
+        {
+          provide: UserService,
+          useValue:fakeUserService,
+        }
+      ]
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -15,4 +36,12 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('test signup and session', async ()=> {
+    let session = {userId: -1}
+    const user = await controller.signUp({email: 'test@test.com', password: 'password'}, session)
+
+    expect(session.userId).toBe(1)
+    expect(user.id).toBe(1)
+  })
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
+  const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+    findOne: jest.fn(entity => entity),
+    // ...
+  }));
+  type MockType<T> = {
+    [P in keyof T]?: jest.Mock<{}>;
+  };
+
   let service: UserService;
+  let repositoryMock: MockType<Repository<User>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: getRepositoryToken(User), useFactory: repositoryMockFactory }
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repositoryMock = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
